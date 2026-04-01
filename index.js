94c3857be817db8a14224b7deb5645775118ad1f
Bogdan3690 <dblevko@gmail.com>
2026-04-01 14:43:49 -0600

    fix stopwatch

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const reset = document.querySelector("[data-action='reset']")
 console.log(output);
 
 let startSeconds = 0;
+let startTime = 0;
 let timerId = null;
 let timerIsActive = false
+let acumulateTime = 0;
 
 start.addEventListener("click", startStopwatch)
 reset.addEventListener("click", resetStopwatch)
@@ -24,14 +26,21 @@ function startStopwatch() {
   if (timerIsActive) {
     return
   }
+  startTime = Date.now()
     timerId = setInterval(() => {
-    startSeconds += 1
-    console.log(startSeconds);
-    const time = getTimeFromSeconds(startSeconds)
-    console.log(time)
-    const timeString = `${time.hours}:${time.minutes}:${time.secs}`
-    output.textContent = timeString
-    console.log(timeString)
+    let currentTime = Date.now()
+    let delta = currentTime - startTime + acumulateTime
+    console.log(currentTime);
+    console.log(delta);
+    const { hours, mins, secs } = getTimeComponents(delta)
+    output.textContent =  `${hours}:${mins}:${secs}`
+    // startSeconds += 1
+    // console.log(startSeconds);
+    // const time = getTimeFromSeconds(startSeconds)
+    // console.log(time)
+    // const timeString = `${time.hours}:${time.minutes}:${time.secs}`
+    // output.textContent = timeString
+    // console.log(timeString)
     }, 1000)
     timerIsActive = true
 }
@@ -40,7 +49,9 @@ function startStopwatch() {
 function resetStopwatch() {
   timerIsActive = false
   clearInterval(timerId)
-  startSeconds = 0
+  startTime = 0
+  acumulateTime = 0
+  // startSeconds = 0
   output.textContent = "00:00:00"
 }
 
@@ -48,30 +59,42 @@ function resetStopwatch() {
 function stopStopwatch() {
   timerIsActive = false
   clearInterval(timerId)
+  let currentTime = Date.now()
+  let delta = currentTime - startTime + acumulateTime
+  acumulateTime = delta
 }
 
 
-function getTimeFromSeconds(startSeconds) {
-  const SECONDS_IN_MINUTE = 60;
-  const SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE; // 3600
-  const SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR; // 86400
+// function getTimeFromSeconds(startSeconds) {
+//   const SECONDS_IN_MINUTE = 60;
+//   const SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE; // 3600
+//   const SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR; // 86400
 
-  const totalSeconds = Math.floor(startSeconds);
+//   const totalSeconds = Math.floor(startSeconds);
 
-  const days = Math.floor(totalSeconds / SECONDS_IN_DAY);
-  const remSeconds = totalSeconds % SECONDS_IN_DAY;
+//   const days = Math.floor(totalSeconds / SECONDS_IN_DAY);
+//   const remSeconds = totalSeconds % SECONDS_IN_DAY;
 
-  const hours = pad(Math.floor(remSeconds / SECONDS_IN_HOUR));
-  const minutes = pad(
-    Math.floor((remSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE)
-  );
-  const secs = pad(remSeconds % SECONDS_IN_MINUTE);
+//   const hours = pad(Math.floor(remSeconds / SECONDS_IN_HOUR));
+//   const minutes = pad(
+//     Math.floor((remSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE)
+//   );
+//   const secs = pad(remSeconds % SECONDS_IN_MINUTE);
 
-  return { days, hours, minutes, secs };
+//   return { days, hours, minutes, secs };
+// }
+
+function getTimeComponents(time) {
+    const hours = pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { hours, mins, secs };
 }
-
-
 
 function pad(value) {
   return String(value).padStart(2, "0");
 }
+
